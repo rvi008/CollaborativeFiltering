b48f88ff5da19a924cb55ece415e603bc1fdfb10
raphael <raphael.vignes@telecom-paristech.fr>
2016-12-14 14:42:19 +0100

    finish

--- commands.scala
+++ commands.scala
@@ -24,3 +24,52 @@ val movies = sc.textFile(new File(movieLensHomeDir, "movies.dat").toString).map 
       (fields(0).toInt, fields(1))
     }.collect().toMap
 
+val numRatings = ratings.count
+val numUsers = ratings.map(_._2.user).distinct.count
+val numMovies = ratings.map(_._2.product).distinct.count
+println("Got " + numRatings + " ratings from " + numUsers + " users on " + numMovies + " movies.")
+
+
+
+
+val training = ratings.filter(x => x._1 < 6).values.repartition(4).cache()
+val validation = ratings.filter(x => x._1 >= 6 && x._1 < 8).values.repartition(4).cache()
+val test = ratings.filter(x => x._1 >= 8).values.cache()
+print(training.count())
+print(validation.count())
+print(test.count())
+val numValidation = validation.count()
+
+
+def computeRmse(model: MatrixFactorizationModel, data: RDD[Rating], implicitPrefs: Boolean) : Double = {
+    def mapPredictedRating(r: Double): Double = {
+      if (implicitPrefs) math.max(math.min(r, 1.0), 0.0) else r
+    }
+    val predictions: RDD[Rating] = model.predict(data.map(x => (x.user, x.product)))
+    val predictionsAndRatings = predictions.map{ x =>
+      ((x.user, x.product), mapPredictedRating(x.rating))
+    }.join(data.map(x => ((x.user, x.product), x.rating))).values
+    math.sqrt(predictionsAndRatings.map(x => (x._1 - x._2) * (x._1 - x._2)).mean())
+  }
+}
+
+val ranks = List(8, 12)
+val lambdas = List(1.0, 10.0)
+val numIters = List(10, 20)
+var bestModel: Option[MatrixFactorizationModel] = None
+var bestValidationRmse = Double.MaxValue
+var bestRank = 0
+var bestLambda = -1.0
+var bestNumIter = -1
+for (rank <- ranks; lambda <- lambdas; numIter <- numIters) {
+val model = ALS.train(training, rank, numIter, lambda)
+val validationRmse = computeRmse(model, validation, true)
+println("RMSE (validation) = " + validationRmse + " for the model trained with rank = "
++ rank + ", lambda = " + lambda + ", and numIter = " + numIter + ".")
+if (validationRmse < bestValidationRmse) {
+        bestModel = Some(model)
+        bestValidationRmse = validationRmse
+        bestRank = rank
+        bestLambda = lambda
+        bestNumIter = numIter}}
+
